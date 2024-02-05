c7c06bf2fef0bd144482b31b44beeb2082b2ac54
ZAA <zaa.1@mail.ru>
2024-02-05 11:15:12 +0300

    /

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from functions import load_data
+from functions import *
 
+data = data_clean()
+data = sort_status(data)
+sorted_data = sort_data(data)
 
-list_operation = load_data('operations.json')
-
-print(list_operation)
-
+for item in sorted_data[:5]:
+    from_to = item.get('from', 'not found')
+    if from_to == 'not found':
+        from_to = from_to
+    elif len(str(from_to.split(" ")[-1])) < 20:
+        from_to = card_hidden(from_to)
+    else:
+        from_to = account_hidden(from_to)
+    date = date_format(item.get('date'))
+    description = item.get("description", 'not found')
+    to = item.get("to", 'not found')
+    if to == 'not found':
+        to = to
+    elif len(str(to.split(" ")[-1])) < 20:
+        to = card_hidden(to)
+    else:
+        to = account_hidden(to)
+    amount = item.get('operationAmount', 'not found').get('amount', 'not found')
+    currency = item.get('operationAmount').get('currency').get('name', 'not found')
+    print(f'{date} {description}\n{from_to} -> {to}\n{amount} {currency} \n')
