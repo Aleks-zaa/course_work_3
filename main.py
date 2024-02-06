341273fd16c528020681fc2f7e0fcd2d89bf5b76
ZAA <zaa.1@mail.ru>
2024-02-06 15:38:57 +0300

    обернул основную логику программы в функцию main и импортировал конкретные функции

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from functions import *
+from functions import data_clean, sort_data, sort_status, card_hidden, account_hidden, date_format
 
 data = data_clean("operations.json")
 data = sort_status(data)
 sorted_data = sort_data(data)
 
-for item in sorted_data[:5]:
+def main():
+    """ Основная логика программы, которая выводит на экран список из 5 последних выполненных клиентом операций"""
+    for item in sorted_data[:5]:
+        from_to = item.get('from', 'not found')
+        if from_to == 'not found':
+            from_to = from_to
+        elif len(str(from_to.split(" ")[-1])) < 20:
+            from_to = card_hidden(from_to)
+        else:
+            from_to = account_hidden(from_to)
+        date = date_format(item.get('date'))
+        description = item.get("description", 'not found')
+        to = item.get("to", 'not found')
+        if to == 'not found':
+            to = to
+        elif len(str(to.split(" ")[-1])) < 20:
+            to = card_hidden(to)
+        else:
+            to = account_hidden(to)
+        amount = item.get('operationAmount', 'not found').get('amount', 'not found')
+        currency = item.get('operationAmount').get('currency').get('name', 'not found')
+        print(f'{date} {description}\n{from_to} -> {to}\n{amount} {currency} \n')
 
-    from_to = item.get('from', 'not found')
-    if from_to == 'not found':
-        from_to = from_to
-    elif len(str(from_to.split(" ")[-1])) < 20:
-        from_to = card_hidden(from_to)
-    else:
-        from_to = account_hidden(from_to)
-    date = date_format(item.get('date'))
-    description = item.get("description", 'not found')
-    to = item.get("to", 'not found')
-    if to == 'not found':
-        to = to
-    elif len(str(to.split(" ")[-1])) < 20:
-        to = card_hidden(to)
-    else:
-        to = account_hidden(to)
-    amount = item.get('operationAmount', 'not found').get('amount', 'not found')
-    currency = item.get('operationAmount').get('currency').get('name', 'not found')
-    print(f'{date} {description}\n{from_to} -> {to}\n{amount} {currency} \n')
+main()
