5051758f537779f2c9244c6df73c9092703673bc
ZAA <zaa.1@mail.ru>
2024-02-11 21:06:12 +0300

    исключил idea и убрал комиты

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from functions import data_clean, sort_status, sort_data, date_format, card_hidd
 
 def main():
     """ Основная логика программы, которая выводит на экран список из 5 последних выполненных клиентом операций"""
-    # f = open('rust.txt', 'r+')
-    # f.truncate(0)
     data = data_clean("operations.json")
     data = sort_status(data)
     sorted_data = sort_data(data)
@@ -27,8 +25,6 @@ def main():
             to = account_hidden(to)
         amount = item.get('operationAmount', 'not found').get('amount', 'not found')
         currency = item.get('operationAmount').get('currency').get('name', 'not found')
-        # with open('rust.txt', 'a') as f:
-        #     f.write(f'{date} {description}\n{from_to} -> {to}\n{amount} {currency} \n')
         print(f'{date} {description}\n{from_to} -> {to}\n{amount} {currency} \n')
 
 
